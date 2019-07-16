d3bf8442f7a0e3b7842d1d2e68481e4bd0568d16
Селезнев Лев Сергеевич <Seleznev.L.S@sberbank.ru>
2019-07-16 20:10:11 +0300

    Удалил неиспользуемый файл

--- Assets/Scripts/GameEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameEvent
-{
-    public const string FLASHLIGHT_CHARGE_CHANGED = "FLASHLIGHT_CHARGE_CHANGED";
-}
