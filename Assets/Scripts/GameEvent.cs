f20948696918a276d51bd399b0fd40bff140619d
Селезнев Лев Сергеевич <Seleznev.L.S@sberbank.ru>
2019-07-16 18:03:59 +0300

    Добавил логику для фонарика

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvent
+{
+    public const string FLASHLIGHT_CHARGE_CHANGED = "FLASHLIGHT_CHARGE_CHANGED";
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text scoreLabel;
+
+    private void onFlashlightChargeChanged(int charge)
+    {
+        scoreLabel.text = charge.ToString() + "%";
+    }
+}
